c26822fa34776dfc3cb3ab0494352851bf557479
Anuj Singh <anujnatwarlali101@gmail.com>
2021-01-16 19:09:37 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,8 +1,50 @@
-TPATH = 'C:/Users/Aditya Dash/Documents/GitHub/Image-Caption-Generator/Dataset/Captions/descriptions.txt'
+from os import listdir
+from PIL import Image,ImageOps
+import numpy as np
+import matplotlib.pyplot as plt
 
-def preprocess_text():
-    with open(PATH, 'r') as file:
-        captions  = [line for line in file]
-        captions  = [ele.strip().split('\t')[-1] for ele in captions]
+PATH = "C:/Users/AMAR/Documents/GitHub/Image-Caption-Generator/Dataset/Images/"
+RPATH = "C:/Users/AMAR/Documents/GitHub/Image-Caption-Generator/Dataset/resized_Dataset/"
 
-        
+imagesList = listdir(PATH)
+
+NEW_DIM = (200,200)
+
+
+
+def loadImages(load_path):
+
+    loadedImages = []
+    for image in imagesList:
+        img = Image.open(load_path+image)
+        loadedImages.append(img)
+        img.close()
+
+    return loadedImages
+
+
+
+def preprocess_Img(load_path,save_path):
+
+    images = loadImages(load_path)                                              #Loading the images from the dataset using loadImages(load_path)
+
+#resizing the loaded images
+
+    pixel_img = []
+    preprocessed_img = []
+    for img in images:
+        img = img.resize(NEW_DIM)
+        img = ImageOps.expand(img,border=50,fill="black")                       #Padding the resized images
+        preprocessed_img.append(img)                                            #converting the padded and resized images into array(pixel values)
+        pixel_img.append(np.asarray(img))
+
+#saving the Preprocessed images
+
+    j=0
+    for i in preprocessed_img:
+        i.save(save_path+imagesList[j],"JPEG")
+        j+=1
+
+    return pixel_img
+
+def load_dataset():
